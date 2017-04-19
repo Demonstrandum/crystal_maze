fa2b95ee8383b542a06113874e11910036b00aad
Demonstrandum <knutsen@jetspace.co>
2017-04-19 23:31:52 +0100

    Planed outwq

--- lib/A*.rb
+++ lib/A*.rb
@@ -1,18 +1,46 @@
 require 'chunky_png'
 
-maze = nil
+image = nil
 
 begin   
-    maze = ChunkyPNG::Image.from_file(ARGV[ARGV.length - 1])
+    image = ChunkyPNG::Image.from_file(ARGV[ARGV.length - 1])
 rescue
     puts "Please supply correct commandline arguments. No image given."
 end
 
-
-def Astar
-    # TODO: Everything.
+def findStart image
+    # ...
 end
 
+def findEnd image
+    # ...
+end
 
-unless maze.nil?
-    Astar()
+class Astar
+    def initialize(maze, start, dest)
+        inf = Float::INFINITY
+        
+        #@firstNode = Node.new(start[0], start[1], -1, -1, -1, -1) ...
+        #@destNode  = ==========dest======dest==================== ...
+
+        @height       = maze.width
+        @width        = maze.height
+        @perimiter    = 2 * height + 2 * width
+        @area = total = height * width
+        
+        
+        @visited  = [false] * total
+        @previous = [nil]   * total
+        @distance = [inf]   * total
+
+        #@unvisited = someHeapFunc() ...
+    end
+end
+
+unless image.nil? 
+    Astar.new.solver(image, findStart image, findEnd image)
+end
+
+class Node
+    # ...
+end
